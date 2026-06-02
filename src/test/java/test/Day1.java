398f8adadef10d151f28308d30ee01292383fcac
syamala-git <syamala.kovvuri1@gmail.com>
2026-06-02 14:27:03 +0200

    create testng xml - while convert to testNG

---
+++ src/test/java/test/Day1.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day1 {
+
+	@Test
+	public void FirstTest() {
+		System.out.println("TestNG - first test");
+	}
+
+	@Test
+	public void SecondTest() {
+		System.out.println("TestNG - second test");
+
+	}
+
+}
